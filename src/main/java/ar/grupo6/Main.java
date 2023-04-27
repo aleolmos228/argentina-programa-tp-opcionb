8a21a9c7091a46bbba57cd9acd6969611f7cc67e
Juan Pérez <damianfrutos@gmail.com>
2023-04-27 12:44:10 -0300

    Se agrega scanner para ingresar al ruta al archivo por consola

--- src/main/java/ar/grupo6/Main.java
+++ src/main/java/ar/grupo6/Main.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        //Scanner sc = new Scanner(System.in);
-        //System.out.print("Ingrese el path completo del archivo: ");
-        //String rutaCompletaArchivo = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el path completo del archivo: ");
+        String rutaCompletaArchivo = sc.nextLine();
 
-        String rutaCompletaArchivo = "C:\\xampp5\\htdocs\\cursos\\java\\tp_ejemplo_profesor\\HarryPotter-data.csv";
+        //String rutaCompletaArchivo = "C:\\xampp5\\htdocs\\cursos\\java\\tp_ejemplo_profesor\\HarryPotter-data.csv";
 
         ProcesarArchivo proc = new ProcesarArchivo(rutaCompletaArchivo);
 
